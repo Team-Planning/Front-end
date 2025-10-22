f950ed7aa673fa70bbff8cba83d89662b5634a70
Yoselin Cornejo <yoselyn.ycr@gmail.com>
2025-10-21 23:26:03 -0300

    mensaje de desarrollo

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Home() {
-  return <div className="flex flex-col h-full lg:pt-14 flex-1">Home</div>;
+  return (
+    <div className="flex flex-col h-full lg:pt-14 flex-1 justify-center items-center">
+      {/* CAMBIO: Reemplazamos el texto "Home" por "En desarrollo" */}
+      <h1 className="text-3xl font-bold text-gray-700">En desarrollo</h1>
+      <p className="text-xl text-gray-500 mt-2">Esta sección estará disponible pronto.</p>
+    </div>
+  );
 }
 
 export default Home
